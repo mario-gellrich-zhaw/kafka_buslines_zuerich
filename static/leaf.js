9ee58f8e194de09d3dc7e9ad3c58056715420a36
mario-gellrich-zhaw <60516564+mario-gellrich-zhaw@users.noreply.github.com>
2025-08-26 07:57:26 +0000

    ...

--- static/leaf.js
+++ static/leaf.js
@@ -1,11 +1,9 @@
 var mymap = L.map('mapid').setView([47.37128, 8.54161], 13);
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFyaW9nemhhdyIsImEiOiJjbHpjanVvbHcwZDZ3MmlyeTEzMTJtbXF5In0.vVfSVeSJbG_cF53qDoSztA', {
-	maxZoom: 18,
-	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
-		'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-	id: 'mapbox/streets-v11',
-	tileSize: 512,
-	zoomOffset: -1
+
+// Use free OpenStreetMap tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(mymap);
 
 mapMarkers1 = [];
@@ -15,41 +13,41 @@ mapMarkers3 = [];
 /* Icons-Settings */
 /* https://github.com/pointhi/leaflet-color-markers  */
 var myIcon = L.divIcon({
-      className: 'my-div-icon',
-      iconSize: [15, 15]
-    });
+    className: 'my-div-icon',
+    iconSize: [15, 15]
+});
 
 var source = new EventSource('/topic/busdata001');
 source.addEventListener('message', function(e){
 
-  console.log('Message');
-  obj = JSON.parse(e.data);
-  console.log(obj);
+    console.log('Message');
+    obj = JSON.parse(e.data);
+    console.log(obj);
 
-  if(obj.busline == '00001') {
-    for (var i = 0; i < mapMarkers1.length; i++) {
-      mymap.removeLayer(mapMarkers1[i]);
+    if(obj.busline == '00001') {
+        for (var i = 0; i < mapMarkers1.length; i++) {
+            mymap.removeLayer(mapMarkers1[i]);
+        }
+        marker1 = L.marker([obj.latitude, obj.longitude], {icon: myIcon}).addTo(mymap);
+        marker1.valueOf()._icon.style.backgroundColor = 'red';
+        mapMarkers1.push(marker1);
     }
-    marker1 = L.marker([obj.latitude, obj.longitude], {icon: myIcon}).addTo(mymap);
-	marker1.valueOf()._icon.style.backgroundColor = 'red';
-    mapMarkers1.push(marker1);
-  }
 
-  if(obj.busline == '00002') {
-    for (var i = 0; i < mapMarkers2.length; i++) {
-      mymap.removeLayer(mapMarkers2[i]);
+    if(obj.busline == '00002') {
+        for (var i = 0; i < mapMarkers2.length; i++) {
+            mymap.removeLayer(mapMarkers2[i]);
+        }
+        marker2 = L.marker([obj.latitude, obj.longitude], {icon: myIcon}).addTo(mymap);
+        marker2.valueOf()._icon.style.backgroundColor = 'gold';
+        mapMarkers2.push(marker2);
     }
-    marker2 = L.marker([obj.latitude, obj.longitude], {icon: myIcon}).addTo(mymap);
-	marker2.valueOf()._icon.style.backgroundColor = 'gold';
-    mapMarkers2.push(marker2);
-  }
 
-  if(obj.busline == '00003') {
-    for (var i = 0; i < mapMarkers3.length; i++) {
-      mymap.removeLayer(mapMarkers3[i]);
+    if(obj.busline == '00003') {
+        for (var i = 0; i < mapMarkers3.length; i++) {
+            mymap.removeLayer(mapMarkers3[i]);
+        }
+        marker3 = L.marker([obj.latitude, obj.longitude], {icon: myIcon}).addTo(mymap);
+        marker3.valueOf()._icon.style.backgroundColor = 'green';
+        mapMarkers3.push(marker3);
     }
-    marker3 = L.marker([obj.latitude, obj.longitude], {icon: myIcon}).addTo(mymap);
-	marker3.valueOf()._icon.style.backgroundColor = 'green';
-    mapMarkers3.push(marker3);
-  }
 }, false);
